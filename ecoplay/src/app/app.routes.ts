bd999d3e35f82d533d12845b36210348d5cd83f8
Henrique-Estrela <henriqueestrela2004@gmail.com>
2026-05-10 23:04:48 -0300

    build - construção da base para o app

---
+++ ecoplay/src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+	{
+		path: '',
+		pathMatch: 'full',
+		loadComponent: () => import('./pages/home/home.page').then((m) => m.HomePage)
+	},
+	{
+		path: '**',
+		loadComponent: () => import('./pages/not-found/not-found.page').then((m) => m.NotFoundPage)
+	}
+];
