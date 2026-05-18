ed57b39ef9a08933ffeb0feb05ce7f668cc1744a
Henrique Santos <henriqueestrela2004@gmail.com>
2026-05-18 19:52:04 -0300

    up

--- ecoplay/src/app/app.routes.ts
+++ ecoplay/src/app/app.routes.ts
@@ -7,6 +7,10 @@ export const routes: Routes = [
 		loadComponent: () => import('./pages/home/home.page').then((m) => m.HomePage)
 	},
 	{
+		path: '',
+		loadChildren: () => import('./pages/auth/auth.routes').then((m) => m.authRoutes)
+	},
+	{
 		path: '**',
 		loadComponent: () => import('./pages/not-found/not-found.page').then((m) => m.NotFoundPage)
 	}
